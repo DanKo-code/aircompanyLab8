a33a9f3510f0f2d1dad08bd95a63497295469411
Danila Kozlyakovsky <danik2003globin@gmail.com>
2023-10-31 01:02:50 +0300

    Add clean js

---
+++ JS_CLEAN/JS/models/ExperimentalTypes.js
@@ -0,0 +1,8 @@
+const ExperimentalTypes = {
+    LIFTING_BODY: 'Lifting Body',
+    HYPERSONIC: 'Hypersonic',
+    HIGH_ALTITUDE: 'High Altitude',
+    VTOL: "VTOL"
+};
+
+module.exports =  ExperimentalTypes ;
