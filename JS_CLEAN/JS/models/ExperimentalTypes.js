02d0a9245bb2183e20c9e04db8e44dd6108d926f
Danila Kozlyakovsky <danik2003globin@gmail.com>
2023-10-31 01:03:14 +0300

    Revert "Add clean js"
    This reverts commit a33a9f3510f0f2d1dad08bd95a63497295469411.

--- JS_CLEAN/JS/models/ExperimentalTypes.js
+++
@@ -1,8 +0,0 @@
-const ExperimentalTypes = {
-    LIFTING_BODY: 'Lifting Body',
-    HYPERSONIC: 'Hypersonic',
-    HIGH_ALTITUDE: 'High Altitude',
-    VTOL: "VTOL"
-};
-
-module.exports =  ExperimentalTypes ;
--- JS_CLEAN/JS/models/СlassificationLevel.js
+++
@@ -1,8 +0,0 @@
-const ClassificationLevel = {
-    UNCLASSIFIED: 'Unclassified',
-    CONFIDENTIAL: 'Confidential',
-    SECRET: 'Secret',
-    TOP_SECRET: 'Top Secret'
-};
-
-module.exports = ClassificationLevel;
